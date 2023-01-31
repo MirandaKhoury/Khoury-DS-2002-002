81c4497cafea7c8f648185de4ecd69e90e39eff4
MirandaKhoury <123335735+MirandaKhoury@users.noreply.github.com>
2023-01-31 13:45:46 -0500

    Update Lab_01_SQL_Query_Fundamentals.sql

--- 01-SQL/Labs/Lab_01_SQL_Query_Fundamentals.sql
+++ 01-SQL/Labs/Lab_01_SQL_Query_Fundamentals.sql
@@ -5,7 +5,7 @@
 -- ------------------------------------------------------------------
 -- 0). First, How Many Rows are in the Products Table?
 -- ------------------------------------------------------------------
-#SELECT * FROM northwind.products;
+SELECT * FROM northwind.products;
 SELECT COUNT(`id`) AS Num_Products FROM northwind.products;
 -- ------------------------------------------------------------------
 -- 1). Product Name and Unit/Quantity
@@ -34,38 +34,55 @@ SELECT list_price
 	, product_name
 FROM northwind.products
 WHERE list_price = (SELECT MIN(list_price) FROM northwind.products)
-	OR list_price = (SELECT MAX(list_price) FROM northwind.products)
+	OR list_price = (SELECT MAX(list_price) FROM northwind.products);
 -- ------------------------------------------------------------------
 -- 5). Product ID, Name & List Price Costing Less Than $20
 -- ------------------------------------------------------------------
-
-
+SELECT id
+	, product_name
+	, list_price
+FROM northwind.products
+WHERE list_price < 20.00
+ORDER BY list_price DESC;
 -- ------------------------------------------------------------------
 -- 6). Product ID, Name & List Price Costing Between $15 and $20
 -- ------------------------------------------------------------------
-
-
-
+SELECT id
+	, product_name
+	, list_price
+FROM northwind.products
+WHERE list_price BETWEEN 15.00 AND 20.00
+ORDER BY list_price DESC;
 -- ------------------------------------------------------------------
 -- 7). Product Name & List Price Costing Above Average List Price
 -- ------------------------------------------------------------------
-
-
+SELECT product_name
+	, list_price
+FROM northwind.products
+WHERE list_price > (SELECT avg(list_price) FROM northwind.products)
+ORDER BY list_price DESC;
 -- ------------------------------------------------------------------
 -- 8). Product Name & List Price of 10 Most Expensive Products 
 -- ------------------------------------------------------------------
-
-
+SELECT product_name
+	, list_price
+FROM northwind.products
+ORDER BY list_price DESC
+LIMIT 10;
 -- ------------------------------------------------------------------ 
 -- 9). Count of Current and Discontinued Products 
 -- ------------------------------------------------------------------
+SELECT COUNT(`id`) AS "Number Current Products"
+FROM northwind.products
+WHERE discontinued = 0;
 
-
+SELECT COUNT(`id`) AS "Number Discontinued Products"
+FROM northwind.products
+WHERE discontinued = 1;
 -- ------------------------------------------------------------------
 -- 10). Product Name, Units on Order and Units in Stock
 --      Where Quantity In-Stock is Less Than the Quantity On-Order. 
 -- ------------------------------------------------------------------
-
 
 
 -- ------------------------------------------------------------------
